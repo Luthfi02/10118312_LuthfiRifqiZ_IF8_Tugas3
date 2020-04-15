5bb0a31cc7a7a836264d054d8a35fddabeaee812
Luthfi02 <luthfi.rz.lrz@gmail.com>
2020-04-15 09:16:48 +0700

    Kalkulator

---
+++ Praktikum4_Kalkulator/src/utama.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luthfi RZ
+ */
+public class utama extends JFrame {
+    public utama()
+    {
+        this.setSize(300, 300);
+        this.setLocation(200,200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void tampilkan()
+    {
+        this.setVisible(true);
+    }
+}
